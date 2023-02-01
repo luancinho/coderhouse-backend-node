167535b72b15f0bdb7b9aacb048ff609fb1bd7f1
Luis Muñoz <luandevtech@gmail.com>
2023-02-01 06:25:44 -0500

    entregable 02 / cambios

--- src/entregable02/app.js
+++ src/entregable02/app.js
@@ -1,23 +1,28 @@
 /* eslint-disable operator-linebreak */
 const path = require('path');
+// eslint-disable-next-line no-unused-vars
+const colors = require('colors');
 const ProductManager = require('./models/ProductManager');
 
 const separador = (str = '-') => console.log(str.repeat(50));
 
 (async () => {
-  console.log('Se creará una instancia de la clase "ProductManager"');
+  console.log(
+    'Se creará una instancia de la clase "ProductManager"'.green.bold.inverse,
+  );
   const manager = new ProductManager(path.join(__dirname, './db/data.json'));
   console.log(manager instanceof ProductManager);
   separador();
 
   console.log(
-    'Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []',
+    'Se llamará “getProducts” recién creada la instancia, debe devolver un arreglo vacío []'
+      .green.bold.inverse,
   );
   const result1 = await manager.getProducts();
   console.log(Array.isArray(result1) && result1.length === 0);
   separador();
 
-  console.log('Se llamará al método “addProduct”');
+  console.log('Se llamará al método “addProduct”'.green.bold.inverse);
   const result2 = await manager.addProduct(
     'producto prueba',
     'Este es un producto prueba',
@@ -30,7 +35,8 @@ const separador = (str = '-') => console.log(str.repeat(50));
   separador();
 
   console.log(
-    'El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE',
+    'El objeto debe agregarse satisfactoriamente con un id generado automáticamente SIN REPETIRSE'
+      .green.bold.inverse,
   );
   const result3 = await manager.getProducts();
   console.log(result3.length === 1);
@@ -40,26 +46,30 @@ const separador = (str = '-') => console.log(str.repeat(50));
   separador();
 
   console.log(
-    'Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado',
+    'Se llamará el método “getProducts” nuevamente, esta vez debe aparecer el producto recién agregado'
+      .green.bold.inverse,
   );
   console.log(await manager.getProducts());
   separador();
 
   console.log(
-    'Se llamará al método “getProductById” y se corroborará que devuelva el producto con el id especificado, en caso de no existir, debe arrojar un error.',
+    'Se llamará al método “getProductById” y se corroborará que devuelva el producto con el id especificado, en caso de no existir, debe arrojar un error.'
+      .green.bold.inverse,
   );
   console.log(await manager.getProductById(id));
   separador();
 
   console.log(
-    'Se llamará al método “updateProduct” y se intentará cambiar un campo de algún producto, se evaluará que no se elimine el id y que sí se haya hecho la actualización.',
+    'Se llamará al método “updateProduct” y se intentará cambiar un campo de algún producto, se evaluará que no se elimine el id y que sí se haya hecho la actualización.'
+      .green.bold.inverse,
   );
   await manager.updateProduct(id, 'code', '75Ao5a78');
   console.log(await manager.getProductById(id));
   separador();
 
   console.log(
-    'Se llamará al método “deleteProduct”, se evaluará que realmente se elimine el producto o que arroje un error en caso de no existir.',
+    'Se llamará al método “deleteProduct”, se evaluará que realmente se elimine el producto o que arroje un error en caso de no existir.'
+      .green.bold.inverse,
   );
   await manager.deleteProduct(id);
   try {
@@ -69,5 +79,5 @@ const separador = (str = '-') => console.log(str.repeat(50));
   }
   separador();
 
-  console.log('*** FIN ***');
+  console.log('*** FIN ***'.red.bold.inverse);
 })();
